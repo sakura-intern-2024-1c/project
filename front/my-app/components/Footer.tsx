44103cc77e2c9b7ac984699e39b01265ef0c234e
arima <arima@arimanoMacBook-Pro.local>
2024-08-15 17:08:03 +0900

    test

--- front/my-app/components/Footer.tsx
+++ front/my-app/components/Footer.tsx
@@ -1,7 +1,20 @@
-
+import {Button} from "@chakra-ui/react"
+import {Spacer, Center} from '@chakra-ui/react'
+import { useRouter } from 'next/router';
 export const Footer = () => {
+	const router = useRouter()
+	const handleClick = () => {
+  alert(123);
+}
 	return (
 		<>
+		<Center m={12}>
+		<Spacer />
+		<Button onClick={()=>{router.push("/Questions")}}>AI</Button>
+		<Spacer />
+		<Button variant='ghost' onClick={()=>{router.push("/QuestionAnswer")}}>過去のQAコーナー</Button>
+		<Spacer />
+		</Center>
 		</>
 	)
 }
