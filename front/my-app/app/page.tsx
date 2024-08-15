e5ac14827007f93cf9a5895f89f322161e52c10b
arima <arima@arimanoMacBook-Pro.local>
2024-08-16 02:50:16 +0900

    aaa

--- front/my-app/app/page.tsx
+++ front/my-app/app/page.tsx
@@ -5,19 +5,28 @@ import {
   Tfoot,
   Tr,
   Th,
+  background,
 } from '@chakra-ui/react'
 import {Spacer,Center} from '@chakra-ui/react'
 type User = {
 	id: number;
 	name: string;
-	status: number;
+	status: string;
 	experience: string;
 };
-const users: User[] = [
-	{ id: 1, name: "tanaka", status: 1, experience: "new" },
-	{ id: 1, name: "yamada", status: 2, experience: "new" },
-	{ id: 1, name: "sato", status: 3, experience: "new" },
+const usersSample: User[] = [
+	{ id: 1, name: "田中 太郎", status: "休憩中", experience: "営業1年目" },
+	{ id: 2, name: "鈴木 一郎", status: "仕事中", experience: "エンジニア2年目" },
+	{ id: 3, name: "高橋 由美", status: "休憩中", experience: "総務5年目" },
+	{ id: 4, name: "山田 健一", status: "仕事中", experience: "営業4年目" },
+	{ id: 5, name: "伊藤 明美", status: "休憩中", experience: "営業1年目" },
+	{ id: 6, name: "中村 翔太", status: "仕事中", experience: "営業4年目" },
+	{ id: 7, name: "渡辺 直子", status: "休憩中", experience: "エンジニア8年目" },
+	{ id: 8, name: "小林 真一", status: "休憩中", experience: "エンジニア3年目" },
+	{ id: 9, name: "加藤 美咲", status: "外出中", experience: "エンジニア1年目" },
+	{ id: 10, name: "吉田 和彦", status:"休憩中", experience: "エンジニア2年目" },
 ]
+
 const Home= ()=> {
 	return (
 		<Center>
@@ -29,80 +38,38 @@ const Home= ()=> {
 				<Th>経験</Th>
 			</Tr>
 		</Thead>
-		<Tbody>
-			<Tr>
-				<Th>田中</Th>
-				<Th>休憩中</Th>
-				<Th>1年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>田中</Th>
-				<Th>休憩中</Th>
-				<Th>1年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-			<Tr>
-				<Th>山田</Th>
-				<Th>外出中</Th>
-				<Th>5年目</Th>
-			</Tr>
-		</Tbody>
+		<UserList users={ usersSample } />
 		</Table>
 		</Center>
 	);
 }
+
+type Props = {
+  users: User[]
+}
+
+const UserList = ({ users }: Props) => {
+  return (
+	  <Tbody>
+		  {users.map((user) => (
+			  <>
+				  <Tr _hover={{ background:"lightgray" }} >
+					  <Th>
+						  { user.name }
+					  </Th>
+					  <Th>
+						  { user.status }
+					  </Th>
+					  <Th>
+						  { user.experience }
+					  </Th>
+				  </Tr>
+			  </>
+		  ))}
+	  </Tbody>
+  )
+}
+
+
+
 export default Home
