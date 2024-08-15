b41b7292c296685ec2bebb6253e905cc4763701b
arima <arima@arimanoMacBook-Pro.local>
2024-08-15 16:19:30 +0900

    ok

--- front/my-app/app/page.tsx
+++ front/my-app/app/page.tsx
@@ -1,7 +1,19 @@
 import { Header } from "@/components/Header"
-export default function Home() {
+type User = {
+	id: number;
+	name: string;
+	status: number;
+	experience: string;
+};
+const users: User[] = [
+	{ id: 1, name: "tanaka", status: 1, experience: "new" },
+	{ id: 1, name: "yamada", status: 2, experience: "new" },
+	{ id: 1, name: "sato", status: 3, experience: "new" },
+]
+const Home= ()=> {
 	return (
 		<>
 		</>
 	);
 }
+export default Home
